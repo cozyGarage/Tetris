cfc3fb9f39d87977c0531cbfc1084892b9c6191c
cozyGarage <personal.trungnguyen@gmail.com>
2023-04-28 15:21:29 +0300

    Add project files.

---
+++ Tetris/Position.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    public class Position
+    {
+        public int Row { get; set; }
+        public int Column { get; set; }
+        public Position(int row, int column)
+        {
+            this.Row = row;
+            this.Column = column;
+        }
+    }
+}
