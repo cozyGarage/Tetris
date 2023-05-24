76b7a129cd7072637d60691e7c534ff013ecd1a5
cozyGarage <e1500953@edu.vamk.fi>
2023-05-24 23:28:24 +0300

    add comment

--- Tetris/Blocks.cs
+++ Tetris/Blocks.cs
@@ -20,42 +20,42 @@ namespace Tetris
             offset = new Position(StartOffset.Row, StartOffset.Column);                                             // offset is a new Position with the StartOffset.Row and StartOffset.Column.
         }
 
-        public IEnumerable<Position> TilePositions()
+        public IEnumerable<Position> TilePositions()                                                                 // public IEnumerable<Position> TilePositions() that returns the tile positions of the block.
         {
-            foreach (Position p in Tiles[rotationState])
+            foreach (Position p in Tiles[rotationState])                                                             // foreach loop that goes through the Tiles[rotationState].
             {
-                yield return new Position(p.Row + offset.Row, p.Column + offset.Column);
+                yield return new Position(p.Row + offset.Row, p.Column + offset.Column);                                // returns a new Position with the p.Row + offset.Row and p.Column + offset.Column.
             }
         }
 
-        public void RotateCW()
+        public void RotateCW()                                                                                      // public void RotateCW() that rotates the block clockwise.
         {
-            rotationState = (rotationState + 1) % Tiles.Length;
+            rotationState = (rotationState + 1) % Tiles.Length;                                                     // rotationState is equal to (rotationState + 1) % Tiles.Length.
         }
 
-        public void RotateCCW()
+        public void RotateCCW()                                                                                     // public void RotateCCW() that rotates the block counter clockwise.
         {
-            if (rotationState == 0)
+            if (rotationState == 0)                                                                                 // if statement that checks if rotationState is equal to 0.
             {
-                rotationState = Tiles.Length - 1;
+                rotationState = Tiles.Length - 1;                                                                   // rotationState is equal to Tiles.Length - 1.
             }
             else
             {
-                rotationState--;
+                rotationState--;                                                                                    // rotationState is equal to rotationState - 1.
             }
         }
 
-        public void Move(int rows, int columns)
+        public void Move(int rows, int columns)                                                                     // public void Move(int rows, int columns) that moves the block.
         {
-            offset.Row += rows;
-            offset.Column += columns;
+            offset.Row += rows;                                                                                     // offset.Row is equal to offset.Row + rows.
+            offset.Column += columns;                                                                               // offset.Column is equal to offset.Column + columns.
         }
 
-        public void Reset()
+        public void Reset()                                                                                         // public void Reset() that resets the block.
         {
-            rotationState = 0;
-            offset.Row = StartOffset.Row;
-            offset.Column = StartOffset.Column;
+            rotationState = 0;                                                                                      // rotationState is equal to 0.
+            offset.Row = StartOffset.Row;                                                                           // offset.Row is equal to StartOffset.Row.
+            offset.Column = StartOffset.Column;                                                                     // offset.Column is equal to StartOffset.Column.
         }
     }
 }
